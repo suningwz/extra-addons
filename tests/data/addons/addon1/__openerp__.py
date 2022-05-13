3516a7b6bd440d6676c206b6132de5a248d8d572
Norlan Ruiz <ruiznorlan@gmail.com>
2022-05-13 01:22:54 -0600

    [FIX] CI / CD

--- tests/data/addons/addon1/__openerp__.py
+++
@@ -1,6 +0,0 @@
-# must be __openerp__.py, for compatibility with 8, 9 tests
-{
-    "name": "addon 1",
-    "author": "Norlan Ruiz",
-    "license": "LGPL-3",
-}
--- tests/scripts/script1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env click-odoo
-from __future__ import print_function
-
-env = env  # noqa
-
-print(env["res.users"].search([("login", "=", "admin")]).login)
